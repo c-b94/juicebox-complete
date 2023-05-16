a0a6a7182bfe138822449ce91ec89764a00e74eb
mheuangsayaseng <maggieheuang@gmail.com>
2023-05-15 22:50:32 -0500

    api/post api/tag

--- api/posts.js
+++ api/posts.js
@@ -1,31 +1,19 @@
-const express = require('express');
+const express = require("express");
 const postsRouter = express.Router();
-const posts = reqiure("getAllPosts");
-const getAllPosts = require("../db"); 
+const { getAllPosts } = require("../db");
 
-postsRouter.post((req, res, next) => {
-  console.log("A request is being made to /posts");
+postsRouter.use((req, res, next) => {
+  console.log("A request is being made to /users");
 
-  res.send({ message: 'hello from /posts!' });
+  next();
+});
+
+postsRouter.get("/", async (req, res) => {
+  const posts = await getAllPosts();
+
+  res.send({
+    posts,
+  });
 });
 
 module.exports = postsRouter;
-
-// const express = require("express");
-// const postsRouter = express.Router();
-// //const { getAllPosts } = require("../db");
-
-// // postsRouter.post((req, res, next) => {
-// //   console.log("A request is being made to /users");
-
-// //   next();
-// // });
-
-// postsRouter.get("/", async (req, res) => {
-//   const users = await getAllPosts();
-//   res.send({
-//     users,
-//   });
-// });
-
-// module.exports = usersRouter;
